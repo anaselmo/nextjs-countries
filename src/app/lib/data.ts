0548e6c60575e51081d37cfaff749e4b7357a567
anaselmo <nacho.moya.montoro@gmail.com>
2024-02-29 11:30:58 +0100

    hardcoded data

---
+++ src/app/lib/data.ts
@@ -0,0 +1,24 @@
+export const fetchCountries = async () => {
+  return [
+    { id: 1, name: 'Afghanistan', abbreviation: 'AF', capital: 'Kabul' },
+    { id: 2, name: 'Åland Islands', abbreviation: 'AX', capital: 'Mariehamn' },
+    { id: 3, name: 'Albania', abbreviation: 'AL', capital: 'Tirana' },
+    { id: 4, name: 'Algeria', abbreviation: 'DZ', capital: 'Algiers' },
+    { id: 5, name: 'American Samoa', abbreviation: 'AS', capital: 'Pago Pago' },
+    // { id: 6, name: 'Andorra', abbreviation: 'AD', capital: 'Andorra la Vella' },
+    // { id: 7, name: 'Angola', abbreviation: 'AO', capital: 'Luanda' },
+    // { id: 8, name: 'Anguilla', abbreviation: 'AI', capital: 'The Valley' },
+    // { id: 9, name: 'Antarctica', abbreviation: 'AQ', capital: 'N/A' },
+    // { id: 10, name: 'Antigua and Barbuda', abbreviation: 'AG', capital: 'St. John\'s' },
+    // { id: 11, name: 'Argentina', abbreviation: 'AR', capital: 'Buenos Aires' },
+    // { id: 12, name: 'Armenia', abbreviation: 'AM', capital: 'Yerevan' },
+    // { id: 13, name: 'Aruba', abbreviation: 'AW', capital: 'Oranjestad' },
+    // { id: 14, name: 'Australia', abbreviation: 'AU', capital: 'Canberra' },
+    // { id: 15, name: 'Austria', abbreviation: 'AT', capital: 'Vienna' },
+    // { id: 16, name: 'Azerbaijan', abbreviation: 'AZ', capital: 'Baku' },
+    // { id: 17, name: 'Bahamas', abbreviation: 'BS', capital: 'Nassau' },
+    // { id: 18, name: 'Bahrain', abbreviation: 'BH', capital: 'Manama' },
+    // { id: 19, name: 'Bangladesh', abbreviation: 'BD', capital: 'Dhaka' },
+    // { id: 20, name: 'Barbados', abbreviation: 'BB', capital: 'Bridgetown' },
+  ]
+}
