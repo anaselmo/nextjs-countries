0c4d71d8a42f202088c3e92cb359d65a968357b6
anaselmo <nacho.moya.montoro@gmail.com>
2024-02-29 13:08:49 +0100

    new countries and pagination

--- src/app/(dashboard)/(countries)/page.tsx
+++ src/app/(dashboard)/(countries)/page.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import Table from '@ui/countries/countries-table';
+import Table from '@ui/countries/table-countries';
 import { Metadata } from 'next';
 import Search from '@/app/ui/search';
+import Pagination from '@ui/countries/pagination-countries';
+import { fetchCountryPages } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: "Countries"
 };
 
-export default function Home() {
+export default async function Page ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query ?? '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCountryPages(query);
+  
   return (
-    <main className="flex flex-1 flex-col border border-red-500">
-      <div className="flex py-10 justify-center items-center border border-red-500">
+    <main className="flex flex-1 flex-col ">
+      <div className="flex py-5 w-1/2 self-center justify-center items-center">
         <Search placeholder="Search countries..."/>
       </div>
-      <div className="flex flex-1 justify-center items-center border border-red-500">
-        <Table query='query' currentPage={1}/>
+      <div className="flex flex-1 justify-center items-center">
+        <Table query={query} currentPage={currentPage}/>
       </div>
-      <div className="justify-center items-center border border-red-500">Country Pages</div>
+      <div className="flex py-5 justify-center items-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </main>
   );
 }
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,24 +1,75 @@
+const countries = [
+  { id: 1, name: 'Afghanistan', abbreviation: 'AF', capital: 'Kabul' },
+  { id: 2, name: 'Åland Islands', abbreviation: 'AX', capital: 'Mariehamn' },
+  { id: 3, name: 'Albania', abbreviation: 'AL', capital: 'Tirana' },
+  { id: 4, name: 'Algeria', abbreviation: 'DZ', capital: 'Algiers' },
+  { id: 5, name: 'American Samoa', abbreviation: 'AS', capital: 'Pago Pago' },
+  { id: 6, name: 'Andorra', abbreviation: 'AD', capital: 'Andorra la Vella' },
+  { id: 7, name: 'Angola', abbreviation: 'AO', capital: 'Luanda' },
+  { id: 8, name: 'Anguilla', abbreviation: 'AI', capital: 'The Valley' },
+  { id: 9, name: 'Antarctica', abbreviation: 'AQ', capital: 'N/A' },
+  { id: 10, name: 'Antigua and Barbuda', abbreviation: 'AG', capital: 'St. John\'s' },
+  { id: 11, name: 'Argentina', abbreviation: 'AR', capital: 'Buenos Aires' },
+  { id: 12, name: 'Armenia', abbreviation: 'AM', capital: 'Yerevan' },
+  { id: 13, name: 'Aruba', abbreviation: 'AW', capital: 'Oranjestad' },
+  { id: 14, name: 'Australia', abbreviation: 'AU', capital: 'Canberra' },
+  { id: 15, name: 'Austria', abbreviation: 'AT', capital: 'Vienna' },
+  { id: 16, name: 'Azerbaijan', abbreviation: 'AZ', capital: 'Baku' },
+  { id: 17, name: 'Bahamas', abbreviation: 'BS', capital: 'Nassau' },
+  { id: 18, name: 'Bahrain', abbreviation: 'BH', capital: 'Manama' },
+  { id: 19, name: 'Bangladesh', abbreviation: 'BD', capital: 'Dhaka' },
+  { id: 20, name: 'Barbados', abbreviation: 'BB', capital: 'Bridgetown' },
+  { id: 21, name: 'Belarus', abbreviation: 'BY', capital: 'Minsk' },
+  { id: 22, name: 'Belgium', abbreviation: 'BE', capital: 'Brussels' },
+  { id: 23, name: 'Belize', abbreviation: 'BZ', capital: 'Belmopan' },
+  { id: 24, name: 'Benin', abbreviation: 'BJ', capital: 'Porto-Novo' },
+  { id: 25, name: 'Bermuda', abbreviation: 'BM', capital: 'Hamilton' },
+  { id: 26, name: 'Bhutan', abbreviation: 'BT', capital: 'Thimphu' },
+  { id: 27, name: 'Bolivia', abbreviation: 'BO', capital: 'Sucre' },
+  { id: 28, name: 'Bonaire, Sint Eustatius and Saba', abbreviation: 'BQ', capital: 'Kralendijk' },
+  { id: 29, name: 'Bosnia and Herzegovina', abbreviation: 'BA', capital: 'Sarajevo' },
+  { id: 30, name: 'Botswana', abbreviation: 'BW', capital: 'Gaborone' },
+  { id: 31, name: 'Brazil', abbreviation: 'BR', capital: 'Brasília' },
+  { id: 32, name: 'British Indian Ocean Territory', abbreviation: 'IO', capital: 'Diego Garcia' },
+  { id: 33, name: 'British Virgin Islands', abbreviation: 'VG', capital: 'Road Town' },
+  { id: 34, name: 'Brunei Darussalam', abbreviation: 'BN', capital: 'Bandar Seri Begawan' },
+  { id: 35, name: 'Bulgaria', abbreviation: 'BG', capital: 'Sofia' },
+  { id: 36, name: 'Burkina Faso', abbreviation: 'BF', capital: 'Ouagadougou' },
+  { id: 37, name: 'Burundi', abbreviation: 'BI', capital: 'Bujumbura' },
+  { id: 38, name: 'Cabo Verde', abbreviation: 'CV', capital: 'Praia' },
+  { id: 39, name: 'Cambodia', abbreviation: 'KH', capital: 'Phnom Penh' },
+  { id: 40, name: 'Cameroon', abbreviation: 'CM', capital: 'Yaoundé' },
+  { id: 41, name: 'Canada', abbreviation: 'CA', capital: 'Ottawa' },
+  { id: 42, name: 'Cayman Islands', abbreviation: 'KY', capital: 'George Town' },
+  { id: 43, name: 'Central African Republic', abbreviation: 'CF', capital: 'Bangui' },
+  { id: 44, name: 'Chad', abbreviation: 'TD', capital: 'N\'Djamena' },
+  { id: 45, name: 'Chile', abbreviation: 'CL', capital: 'Santiago' },
+  { id: 46, name: 'China', abbreviation: 'CN', capital: 'Beijing' },
+  { id: 47, name: 'Christmas Island', abbreviation: 'CX', capital: 'Flying Fish Cove' },
+  { id: 48, name: 'Cocos (Keeling) Islands', abbreviation: 'CC', capital: 'West Island' },
+  { id: 49, name: 'Colombia', abbreviation: 'CO', capital: 'Bogotá' },
+  { id: 50, name: 'Comoros', abbreviation: 'KM', capital: 'Moroni' },
+];
+
 export const fetchCountries = async () => {
-  return [
-    { id: 1, name: 'Afghanistan', abbreviation: 'AF', capital: 'Kabul' },
-    { id: 2, name: 'Åland Islands', abbreviation: 'AX', capital: 'Mariehamn' },
-    { id: 3, name: 'Albania', abbreviation: 'AL', capital: 'Tirana' },
-    { id: 4, name: 'Algeria', abbreviation: 'DZ', capital: 'Algiers' },
-    { id: 5, name: 'American Samoa', abbreviation: 'AS', capital: 'Pago Pago' },
-    // { id: 6, name: 'Andorra', abbreviation: 'AD', capital: 'Andorra la Vella' },
-    // { id: 7, name: 'Angola', abbreviation: 'AO', capital: 'Luanda' },
-    // { id: 8, name: 'Anguilla', abbreviation: 'AI', capital: 'The Valley' },
-    // { id: 9, name: 'Antarctica', abbreviation: 'AQ', capital: 'N/A' },
-    // { id: 10, name: 'Antigua and Barbuda', abbreviation: 'AG', capital: 'St. John\'s' },
-    // { id: 11, name: 'Argentina', abbreviation: 'AR', capital: 'Buenos Aires' },
-    // { id: 12, name: 'Armenia', abbreviation: 'AM', capital: 'Yerevan' },
-    // { id: 13, name: 'Aruba', abbreviation: 'AW', capital: 'Oranjestad' },
-    // { id: 14, name: 'Australia', abbreviation: 'AU', capital: 'Canberra' },
-    // { id: 15, name: 'Austria', abbreviation: 'AT', capital: 'Vienna' },
-    // { id: 16, name: 'Azerbaijan', abbreviation: 'AZ', capital: 'Baku' },
-    // { id: 17, name: 'Bahamas', abbreviation: 'BS', capital: 'Nassau' },
-    // { id: 18, name: 'Bahrain', abbreviation: 'BH', capital: 'Manama' },
-    // { id: 19, name: 'Bangladesh', abbreviation: 'BD', capital: 'Dhaka' },
-    // { id: 20, name: 'Barbados', abbreviation: 'BB', capital: 'Bridgetown' },
-  ]
+  return countries
+}
+
+const ITEMS_PER_PAGE = 6;
+export const fetchFilteredCountries = async (
+  query: string,
+  currentPage: number,
+) => {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(query.toLowerCase()),
+  );
+  return filteredCountries.slice(offset, offset + ITEMS_PER_PAGE);
+}
+
+export const fetchCountryPages = async (query: string) => {
+  const { length } = countries.filter((country) =>
+    country.name.toLowerCase().includes(query.toLowerCase()),
+  );
+  return Math.ceil(length / ITEMS_PER_PAGE)
 }
